0d44a52af6432d16b4de3f26afea258567cf7475
simone <simone@simone-ubuntu>
2021-01-14 12:23:48 +0100

    Creata entity cart

---
+++ home-health-care/src/main/java/it/univpm/hhc/model/entities/Cart.java
@@ -0,0 +1,58 @@
+package it.univpm.hhc.model.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="cart")
+public class Cart implements Serializable {
+	private Long cart_id;
+	private Long user_code;
+	private double total;
+	private int item_num;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	public Long getCart_id() {
+		return cart_id;
+	}
+	
+	public void setCart_id(Long cart_id) {
+		this.cart_id = cart_id;
+	}
+	
+	@Column
+	public Long getUser_code() {
+		return user_code;
+	}
+	
+	public void setUser_code(Long user_code) {
+		this.user_code = user_code;
+	}
+	
+	@Column
+	public double getTotal() {
+		return total;
+	}
+	
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	
+	@Column
+	public int getItem_num() {
+		return item_num;
+	}
+	
+	public void setItem_num(int item_num) {
+		this.item_num = item_num;
+	}
+	
+}
