cfe2f8ff5e248e8a8d0485bf94c603b70e090c36
CristianColavito <cristian.colavito@gmail.com>
2021-01-14 16:04:26 +0100

    Creata entità User

--- home-health-care/src/main/java/it/univpm/hhc/model/entities/User.java
+++ home-health-care/src/main/java/it/univpm/hhc/model/entities/User.java
@@ -1,11 +1,14 @@
 package it.univpm.hhc.model.entities;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -16,40 +19,85 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "USERS")
 public class User implements Serializable {
+	private Long user_id;
+	private Long sub_code;
+	private Long address_code;
+	private String password;
+	private String email;
+	private String name;
+	private String surname;
+	private Date subexp; //data di scadenza dell'abbonamento
+	private Boolean role; // 0=user 1=admin
+	private Boolean active; // è is_enable del prof
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "USER_ID")
+	public Long getUser_id() {
+		return user_id;
+	}	
+
+	@Column(name = "PASSWORD", nullable = false) //nullable serve per i campi non vuoti, in questo modo dico che la pass è obbligatoria
+	public String getPassword() {
+		return password;
+	}
 	
-  @Id
-  @Column(name = "USERNAME")
-  private String username;
-
-  @Column(name = "PASSWORD", nullable = false)
-  private String password;
-
-  @Column(name = "ENABLED", nullable = false)
-  private boolean enabled;
-
-  
-  public String getUsername() {
-	  return this.username;
-  }
-  
-  public void setUsername(String username) {
-	  this.username = username;
-  }
-  
-  public String getPassword() {
-	  return this.password;
-  }
-  
-  public void setPassword(String password) {
-	  this.password = password;
-  }
-  
-  public boolean isEnabled() {
-	  return this.enabled;
-  }
-  
-  public void setEnabled(boolean enabled) {
-	  this.enabled = enabled;
-  } 
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Column(name = "EMAIL", nullable = false) 
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Column(name = "NAME", nullable = false) 
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Column(name = "SURNAME", nullable = false) 
+	public String getSurname() {
+		return surname;
+	}
+	
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	@Column(name = "SUBSCRIPTION_EXPIRATION") 
+	public Date getSubexp() {
+		return subexp;
+	}
+	
+	public void setSubexp(Date subexp) {
+		this.subexp= subexp;
+	}
+	
+	@Column(name = "ROLE", nullable = false)
+	public boolean getRole() {
+		  return this.role;
+	  }
+	  
+	public void setRole(boolean role) {	  
+		this.role= role;
+	}
+	
+	@Column(name = "ACTIVE", nullable = false)
+	public boolean isActive() {
+		  return this.active;
+	  }
+	  
+	public void setEnabled(boolean active) {	  
+		this.active = active;
+	}
   
 }
