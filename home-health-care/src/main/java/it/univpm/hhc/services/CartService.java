ca59bc4a05b21b3a5e59fa907858c94bb5389990
simone <simone@simone-ubuntu>
2021-01-16 12:08:08 +0100

    Service per cart

---
+++ home-health-care/src/main/java/it/univpm/hhc/services/CartService.java
@@ -0,0 +1,20 @@
+package it.univpm.hhc.services;
+
+import java.util.List;
+
+import it.univpm.hhc.model.entities.Cart;
+import it.univpm.hhc.model.entities.User;
+
+public interface CartService {
+	
+	List <Cart> findAll();
+	
+	Cart findById(Long id);
+	
+	Cart create (int item_num, double total, User user);
+	
+	Cart update(Cart cart);
+	
+	void delete(Long cartId);
+
+}
---
+++ home-health-care/src/main/java/it/univpm/hhc/services/CartServiceDefault.java
@@ -0,0 +1,45 @@
+package it.univpm.hhc.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.univpm.hhc.model.dao.CartDao;
+import it.univpm.hhc.model.entities.Cart;
+import it.univpm.hhc.model.entities.User;
+
+@Transactional
+@Service("cartService")
+public class CartServiceDefault implements CartService {
+	
+	CartDao cartDao;
+
+	@Override
+	public List<Cart> findAll() {
+		return this.cartDao.findAll();
+	}
+
+	@Override
+	public Cart findById(Long id) {
+		return this.cartDao.findById(id);
+	}
+
+	@Override
+	public Cart create(int item_num, double total, User user) {
+		return this.cartDao.create(item_num, total, user);
+	}
+
+	@Override
+	public Cart update(Cart cart) {
+		return this.cartDao.update(cart);
+	}
+
+	@Override
+	public void delete(Long cartId) {
+		Cart cart = this.cartDao.findById(cartId);
+		this.cartDao.delete(cart);
+	}
+	
+
+}
