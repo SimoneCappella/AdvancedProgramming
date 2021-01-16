e4e7d110cf5e2a66dc48495eb9e4400231608635
simone <simone@simone-ubuntu>
2021-01-16 17:23:10 +0100

    Creato controller Cart con metodo list (vista non collegata)

--- home-health-care/src/main/java/it/univpm/hhc/controller/CartController.java
+++ home-health-care/src/main/java/it/univpm/hhc/controller/CartController.java
@@ -1,5 +1,25 @@
 package it.univpm.hhc.controller;
 
+
+import java.util.List;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ch.qos.logback.classic.Logger;
+import it.univpm.hhc.model.entities.Cart;
+import it.univpm.hhc.services.CartService;
+
+@RequestMapping("/cart")
 public class CartController {
 
+	private CartService cartService;
+	
+	@GetMapping(value = "/list")
+	public String list (Model uiModel) {
+		List<Cart> cart = this.cartService.findAll();
+		uiModel.addAttribute("cart", cart );
+		return "cart/list";
+	}
+	
 }
