117094501de37ba8c2e2886ea823cc9a1fd86ff9
simone <simone@simone-ubuntu>
2021-01-16 11:40:41 +0100

    Creati Dao e DaoDefault per Cart

---
+++ home-health-care/src/main/java/it/univpm/hhc/model/dao/CartDao.java
@@ -0,0 +1,24 @@
+package it.univpm.hhc.model.dao;
+
+import org.hibernate.Session;
+import java.util.List;
+import it.univpm.hhc.model.entities.Cart;
+import it.univpm.hhc.model.entities.User;
+
+public interface CartDao {
+
+	Session getSession();
+	
+	public void setSession(Session session);
+	
+	Cart findById(Long id);
+	
+	List <Cart> findAll();
+	
+	Cart create(int item_num, double total, User user);
+	
+	Cart update(Cart cart);
+	
+	void delete(Cart cart);
+	
+}
---
+++ home-health-care/src/main/java/it/univpm/hhc/model/dao/CartDaoDefault.java
@@ -0,0 +1,45 @@
+package it.univpm.hhc.model.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.mysql.cj.Session;
+
+import it.univpm.hhc.model.entities.Cart;
+import it.univpm.hhc.model.entities.User;
+
+@Repository("cartDao")
+public class CartDaoDefault extends DefaultDao implements CartDao {
+	
+	@Override
+	public Cart findById(Long id) {
+		return getSession().find(Cart.class, id);
+	}
+
+	@Override
+	public List<Cart> findAll() { 
+		return getSession().createQuery("from cart c",Cart.class).getResultList();
+	}
+
+	@Override
+	public Cart create(int item_num, double total, User user) {
+		Cart c = new Cart();
+		c.setItem_num(item_num);
+		c.setTotal(total);
+		c.setUser(user); //potrebbe dare errore (come passargli uno user?)
+		this.getSession().save(c);
+		return c;
+	}
+
+	@Override
+	public Cart update(Cart cart) {
+		return (Cart) this.getSession().merge(cart);
+	}
+
+	@Override
+	public void delete(Cart cart) {
+		this.getSession().delete(cart);
+	}
+	
+}
