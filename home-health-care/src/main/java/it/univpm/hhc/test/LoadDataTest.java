25aebff7324a8893004fa6026c200e5f6d47a0fc
CristianColavito <cristian.colavito@gmail.com>
2021-03-14 19:25:08 +0100

    Imagini e test finali

--- home-health-care/src/main/java/it/univpm/hhc/test/LoadDataTest.java
+++ home-health-care/src/main/java/it/univpm/hhc/test/LoadDataTest.java
@@ -54,9 +54,17 @@ public class LoadDataTest {
 				
 				session.beginTransaction();
 
-				itemDao.create("Patate","ti riempiono tanto tanto",5,"media/patate.jpg");
-				itemDao.create("Libro","utile per fermare le carte",10,"media/libro.jpg");
-				itemDao.create("Mattone","un bel mattone per la tua casa",15,"media/logo_transparent.png");
+				itemDao.create("Siringa","Siringa di plastica usa e getta da 5ml",1,"media/siringa.jfif");
+				itemDao.create("Tachipirina","Blister di 10 pasticche da 1000mg",5,"media/tachipirina.jpg");
+				itemDao.create("Servizio Tampone Rapido","Test Rapido per rilevare la presenza di COVID-19 effettuato da un operatore sanitario",20,"media/tamp_rapid.jfif");
+				itemDao.create("Servizio Tampone Molecolare","Test Molecolare per rilevare la presenza di COVID-19 effettuato da un operatore sanitario",40,"media/tamp_mole.jfif");
+				itemDao.create("Mascherine pack 20","Pack da 20 mascherine usa e getta fp2 ",20,"media/mascherine.jfif");
+				
+				itemDao.create("Penne 500g","Confezione da 500g di penne rigate",1,"media/penne.jpg");
+				itemDao.create("Spaghetti 500g","Confezione da 500g di spaghetti",1,"media/spaghetti.png");
+				itemDao.create("Sugo al basilico 500g","singola confezione sugo al basilico da 500g",3,"media/sugo.jfif");
+				itemDao.create("Patate 1Kg","Sacchetto di patate novelle",1.5,"media/patate.jpg");
+				itemDao.create("Acqua","Confezione di 6 bottiglie da 2l di acqua naturale oligominerale San Benedetto",3.20,"media/acqua.jpg");
 				
 				List<Item> all= itemDao.findAll();
 				session.getTransaction().commit();
@@ -65,30 +73,38 @@ public class LoadDataTest {
 				// phase 3 : create user
 				session.beginTransaction();
 				
-				User u1 = userDao.createbytest(userDao.encryptPassword("user1"), "user1","cristiano","ronaldo");
+				User u1 = userDao.createbytest(userDao.encryptPassword("password"), "admin1@gmail.com","Cristiano","Ronaldo");
+				u1.setRole(true);
+				User u2 = userDao.createbytest(userDao.encryptPassword("password"), "admin2@gmail.com","Giorgia","Marone");
 				u1.setRole(true);
 			
 				Cart c1= cartDao.create(0, 0, u1);
-				Cart c2= cartDao.create(0, 0, userDao.create("user2", "user2","matteo","bianchi"));
-				Cart c3= cartDao.create(0, 0, userDao.create("user3", "user3","franco","rossi"));
+				Cart c2= cartDao.create(0, 0, u2);
+				Cart c3= cartDao.create(0, 0, userDao.create("password", "user1@gmail.com","Matteo","Bianchi"));
+				Cart c4= cartDao.create(0, 0, userDao.create("password", "user2@gmail.com","Franco","Rossi"));
+				Cart c5= cartDao.create(0, 0, userDao.create("password", "user3@gmail.com","Luca","Neri"));
 				
 				//Cart c3= cartDao.create(0, 0, null);
 				//c3.addUser(u3);
 				
 				
-				Sub s1=subDao.create("Poveri", 20.20, 2);
+				Sub s1=subDao.create("Standard", 15D, 10);
 				subDao.update(s1);
-				Sub s2=subDao.create("ricchi", 29.20, 20);
+				Sub s2=subDao.create("Premium", 30D, 30);
 				subDao.update(s2);
 				
 				assert( c1.getTotal() == 10);
 				cartDao.update(c1);
 				cartDao.update(c2);
 				cartDao.update(c3);
+				cartDao.update(c4);
+				cartDao.update(c5);
 				
-				addressDao.create("62017","Citta 1","Via 1","12",c1.getUser());
-				addressDao.create("62018","Citta 2","Via 2","13",c2.getUser());
-				addressDao.create("62019","Citta 2","Via 2","14",c3.getUser());
+				addressDao.create("60122","Ancona","Via Giulio Cesare","12",c1.getUser());
+				addressDao.create("60122","Ancona","Via Caio","69",c2.getUser());
+				addressDao.create("60123","Ancona","Via Franco Romano","23",c2.getUser());
+				addressDao.create("00123","Roma","Via Del Tricolore","68 A",c4.getUser());
+				addressDao.create("00119","Roma","Via Colosseo","40 C",c3.getUser());
 				
 				List<Address> allAddress= addressDao.findAll();
 				
